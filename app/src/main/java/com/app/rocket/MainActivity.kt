e04d99fff326c1a3d5474b382e934a423d9d27a0
Derrick Henry <derrick16henry@gmail.com>
2022-06-27 14:36:13 -0400

    init commit

---
+++ app/src/main/java/com/app/rocket/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.app.rocket
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+}
